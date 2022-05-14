15b99fc2bbd6a0a7fc0e1b6e3d2ef0fd58237feb
YishaiDav <105463122+YishaiDav@users.noreply.github.com>
2022-05-13 20:35:14 -0500

    Update MemoryGame.py

--- MemoryGame.py
+++ MemoryGame.py
@@ -2,11 +2,16 @@ from random import *
 from turtle import *
 from freegames import path
 
+"Importamos los módulos" 
+
+"Colocamos la pantalla"
 screen = Screen()
  
-
+"Escoger el color del fondo"
 screen.bgcolor("blue")
 
+
+"Se define la funcion para crear la seccion de cuadros dentro del juego"
 def Square(x, y):
     up()
     goto(x, y)
@@ -17,15 +22,17 @@ def Square(x, y):
         forward(50)
         left(90)
     end_fill()
- 
+ "Se determina el rango del tablero, como tambien el color"
 
+"Definir el conteo para mantener el numero indice"
 def Numbering(x, y):
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
  
-
 def Coordinates(count):
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
  
+ 
+ "Se define la funcion para hacer que el usuario pueda hacer click"
 def click(x, y):
     spot = Numbering(x, y)
     mark = state['mark']
@@ -36,6 +43,7 @@ def click(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+ "Determinamos el numero de veces que se da click y que si es una respuesta incorrecta, este vuelva a ocultar donde se dio click"
  
 def draw():
     clear()
@@ -63,7 +71,7 @@ tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
  
-
+"Usamos lo siguienre para barajar los cuadros a seleccionar" 
 shuffle(tiles)
 tracer(False)
 onscreenclick(click)
